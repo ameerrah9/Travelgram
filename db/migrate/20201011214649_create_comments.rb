fa968c0abcc7f215ef855b884be5b099606642ee
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-10-11 17:57:36 -0400

    Change Migration File

--- db/migrate/20201011214649_create_comments.rb
+++ db/migrate/20201011214649_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.string :content
-      t.resources :user
-      t.resources :post
+      t.references :user, null: false, foreign_key: true
+      t.references :trip, null: false, foreign_key: true
 
       t.timestamps
     end
