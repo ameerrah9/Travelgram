5905e381b05497444378db0bd58d68bc9501e0ea
ameerrah9 <mccullers.ghameerah@gmail.com>
2020-10-11 17:58:51 -0400

    Migration

--- db/migrate/20201011215231_create_cities.rb
+++ db/migrate/20201011215231_create_cities.rb
@@ -2,8 +2,7 @@ class CreateCities < ActiveRecord::Migration[6.0]
   def change
     create_table :cities do |t|
       t.string :name
-      t.references :user, null: false, foreign_key: true
-
+      
       t.timestamps
     end
   end
